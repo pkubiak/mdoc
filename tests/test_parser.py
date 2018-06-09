115f47fa3c4ef1beb5974f50aafebab3523a4e69
Paweł Kubiak <pawel.kubiak.it@gmail.com>
2018-06-09 03:00:37 +0200

    Removed old tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,102 +4,6 @@ from parameterized import parameterized
 from mdoc.parser import tokenize
 
 
-# class ParserTestCase(unittest.TestCase):
-#     @parameterized.expand([
-#         # ('name', '/* comment */', ''),
-#         # ('multline-02', 'hello/* comment */ world', 'hello world'),
-#         # ('multiline-with-inline', '/* hello // sadad */', ''),
-#         # ('multiline-with-newline', "4/* Hello \n\n\n\nWordl\n*/2", '42'),
-#         # ('multiple-multiline', 'H/* 123 */el/*456*/lo Wo/* 789 */rld', 'Hello World'),
-#         # ('', ";$asd: 'asdas;d'", ...),
-#         # ('string-may-contain-whitespace', '$x: "   "', '...'),
-#         # ('string-variable-interpolation', '$x: "#{date}"', '...')
-#         ### COMMENTS ###
-#
-#         ### VARIABLES ###
-#
-#         ### WIDGETS ###
-#
-#         #
-#         ('widget-without-params', '%widget', [('WIDGET', 'widget', {}, None)]),
-#
-#         ('single-line-empty-widget-01', '@image()', [('WIDGET', 'image', {}, None)]),
-#
-#         ('single-line-empty-widget-02', '@image(     \t)', [('WIDGET', 'image', {}, None)]),
-#
-#         ('widget-with-param-01', '@image(src="img.jpg")', [('WIDGET', 'image', {'src': ('STRING', 'img.jpg')}, None)]),
-#
-#         ('widget-with-param-02-whitespaces', '@image(  src =   "img.jpg"  )', [('WIDGET', 'image', {'src': ('STRING', 'img.jpg')}, None)]),
-#
-#         ('widget-with-param-03-multiline', '@image(\n\tsrc="img.jpg"\n)', [('WIDGET', 'image', {'src': ('STRING', 'img.jpg')}, None)]),
-#
-#         ('single-line-widget-with-params',
-#          '@image(src="img.jpg", margin=5, border=true)',
-#          [('WIDGET', 'image', {'src': ('STRING', 'img.jpg'), 'margin': ('INTEGER', 5), 'border': ('BOOLEAN', True)}, None)]),
-#
-#         ('multi-line-widget-with-params',
-#          '@image(\n  src="img.jpg",\n  margin=5,\n  border=true\n)',
-#          [('WIDGET', 'image', {'src': ('STRING', 'img.jpg'), 'margin': ('INTEGER', 5), 'border': ('BOOLEAN', True)}, None)]),
-#
-#         ('widget-with-identifier', '@image(src = $src)', [('WIDGET', 'image', {'src': ('IDENTIFIER', 'src')}, None)]),
-#         ('widget-with-empty-block', '@image() {\n}', [('WIDGET', 'image', {}, '')]),
-#
-#         ### PARAGRAPHS ###
-#     ])
-#     def test_parser(self, name, text, expected):
-#         self.assertEqual(parser.parse(text), expected, name)
-#
-#     def test_01(self):
-#         self.assertTokenizer([
-#             '%widget'
-#         ], [
-#             ('WIDGET', '')
-#         ])
-
-#
-# class TokenizeWidgetTestCase(unittest.TestCase):
-#     """
-#
-#     """
-#     def assertTokenize(self, inputs, output):
-#         if not all(isinstance(i, list) for i in inputs):
-#             inputs = [inputs]
-#         for input in inputs:
-#             self.assertEqual(tokenize("\n".join(input)), output)
-#
-#     @parameterized.expand([
-#         ('%widget\n', True),
-#         ('%widget()\n', True),
-#         ('%widget() {\n',
-#           '}'])
-#
-#     ])
-#     def test_without_params_and_body(self, input, is_ok):
-#         """
-#         When widget doesn't have params and body, paranthesis can be omited but they mustn't
-#         """
-#         output = [('WIDGET', 'widget', {}, None)]
-#
-#         self.assertTokenize([
-#             [
-#             '%widget'
-#         ], output)
-#
-#         self.assertTokenize([
-#             '%widget()'
-#         ], output)
-#
-#         self.assertTokenize([
-#             '%widget() {',
-#             '}'
-#         ], output)
-#
-#
-#         self.assertSyntaxError([
-#             '%widget {}',
-#         ])
-
-
 class TokenizeTestCase(unittest.TestCase):
     @parameterized.expand([
         ### COMMENTS ###
@@ -197,7 +101,7 @@ class TokenizeTestCase(unittest.TestCase):
         #### smartarrows ####
         #### emoji ####
 
-        
+
         #### LATEX $$xxx$$ ####
 
         ### ANCHORS ###
